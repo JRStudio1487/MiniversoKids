0ce86287f760d511979920dbaac9a44ff1fc2504
JRStudio1487 <jrdesarrollo@hotmail.com>
2025-05-28 13:26:20 +0200

    Matizando about us

--- js/main.js
+++ js/main.js
@@ -95,32 +95,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // Mensaje de agradecimiento tras envio mensaje contacto
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contact-form');
-  const thanks = document.getElementById('contact-thanks');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const form = document.getElementById('contact-form');
+//   const thanks = document.getElementById('contact-thanks');
 
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();  // evitamos recarga
+//   form.addEventListener('submit', async (e) => {
+//     e.preventDefault();  // evitamos recarga
 
-    const data = new FormData(form);
-    try {
-      const res = await fetch(form.action, {
-        method: form.method,
-        body: data,
-        headers: { 'Accept': 'application/json' }
-      });
-      if (res.ok) {
-        form.style.display = 'none';
-        thanks.style.display = 'block';
-      } else {
-        console.error('Error enviando el form:', res.statusText);
-        // aquí podrías mostrar un mensaje de error
-      }
-    } catch (err) {
-      console.error('Fetch error:', err);
-    }
-  });
-});
+//     const data = new FormData(form);
+//     try {
+//       const res = await fetch(form.action, {
+//         method: form.method,
+//         body: data,
+//         headers: { 'Accept': 'application/json' }
+//       });
+//       if (res.ok) {
+//         form.style.display = 'none';
+//         thanks.style.display = 'block';
+//       } else {
+//         console.error('Error enviando el form:', res.statusText);
+//         // aquí podrías mostrar un mensaje de error
+//       }
+//     } catch (err) {
+//       console.error('Fetch error:', err);
+//     }
+//   });
+// });
 
 // GALERIA
 document.addEventListener('DOMContentLoaded', () => {
